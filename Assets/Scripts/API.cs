0d431fb6620e371fbc7fbe4610da2554ae981a1b
freddiezeb <Freddiezeb@hotmail.com>
2018-04-15 19:09:13 +0200

    Added base project with android settings and a basic API test

---
+++ Assets/Scripts/API.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class API : MonoBehaviour {
+
+    // Use this for initialization
+
+    private const string URL = "http://www.omdbapi.com/?t=avengers&type=movie&apikey=e15d9329";
+    public Text responseText;
+
+    public void Request()
+    {
+        WWW request = new WWW(URL);
+        StartCoroutine(OnResponse(request));
+    }
+
+    private IEnumerator OnResponse(WWW req)
+    {
+        yield return req;
+
+        responseText.text = req.text;
+    }
+}
